05b076e246ddd83ef148589d873fe9cb26afba80
Shiiroyashaaa <stevenvang2011@hotmail.com>
2022-06-03 12:18:14 -0500

    Added folders and organized files

---
+++ Python/Programs/program1.py
@@ -0,0 +1,35 @@
+
+state = "Minnesota"
+country = "United States"
+landOLakes = "the Land of 10,000 Lakes"
+temp = "Fahrenheit"
+tower = "February 2, 1996"
+moorhead = "July 6, 1936"
+
+population = int(5379139)
+year = int(2012)
+highDegree = int(174)
+secondHigh = int(114)
+negativeDegree = int(-60)
+rate1 = float(5.35)
+rate2 = float(7.05)
+rate3 = float(7.85)
+rate4 = float(9.85)
+
+print(state + " is a state in the Midwestern " + country + ". Owing its large number of lakes, the state is informally know as \"" + landOLakes + "\".\nThe " + country + 
+" Census Bureau estimates that the population of " + state + " was " + str(population) + " in " + str(year) + ".\n")
+
+print(state + " experiences temperature extremes characteristic of its continental climate, with cold winters and hot summers.\nThe record high and low span is " + str(highDegree) + " degrees " 
++ temp + ", from " + str(negativeDegree) + " " + temp + " at Tower on " + tower + ", to " + str(secondHigh) + temp + " at Moorhead on " + moorhead + ". \n")
+
+print(state + " has a progressive income tax structure; the four brackets of state income tax rates are " + str(rate1) + ", " + str(rate2) + ", " + str(rate3) + " and " 
++ str(rate4) + " percent.")
+
+
+
+
+
+
+
+
+
